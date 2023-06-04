28e0349f1323aa5498d45c8100f295de62ddda4b
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-06-03 19:50:25 -0700

    change link to navlink

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
     return (
         <div>
             <nav>
-                <Link to="/">Home</Link>{' '}
-                <Link to="/movies">Movies</Link>
+                <NavLink to="/">Home</NavLink>{' '}
+                <NavLink to="/movies">Movies</NavLink>
             </nav>
         </div>
     );
