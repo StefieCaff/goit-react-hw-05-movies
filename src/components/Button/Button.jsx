3315584f63f87f7d8355d46cec5ecf95ba6a0b99
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-07-03 10:06:22 -0700

    delelete unused vars

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,7 @@
-import { useLocation } from "react-router-dom";
 import { StyledGoBackLink } from "./styled-button";
 
 const GoBackButton = ({ className }) => {
-  const location = useLocation();
- 
+   
   return (
     <div className={className}>
       <StyledGoBackLink>Back</StyledGoBackLink>
