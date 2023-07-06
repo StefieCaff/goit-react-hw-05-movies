47f4c0e106fdd511549265803fb1807fad6f66de
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-07-06 08:50:44 -0700

    Update Button.jsx
    goBackButton bug fix

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,18 @@
-import { StyledGoBackLink } from "./styled-button";
-import { useLocation } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
+
+import { StyledGoBackLink } from './styled-button';
+
 const GoBackButton = ({ className }) => {
   const location = useLocation();
+  const goBackPath = location.state?.from || '/movies';
+
   return (
     <div className={className}>
-      <StyledGoBackLink to={location.state.from} state={{ from: location.pathname }}>Back</StyledGoBackLink>
-    </div>);
+      <StyledGoBackLink to={goBackPath} state={{ from: location.pathname }}>
+        Back
+      </StyledGoBackLink>
+    </div>
+  );
 };
 
 export default GoBackButton;
